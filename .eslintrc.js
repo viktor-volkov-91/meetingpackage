3a75266769e4fb592c2bac64f2a3c422feae874b
viktorvolkov <viktor.volkov@peertown.com>
2023-04-02 13:07:59 +0400

    Add monorepo

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript'
+  ],
+  ignorePatterns: ['**/dist/*', '**/build/*', '**/node_modules/*']
+};
